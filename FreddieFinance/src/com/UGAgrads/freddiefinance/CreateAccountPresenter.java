e730a7107550e6b2d837b6a4a00e93edd499362e
Brandon Foley <brandonfoley13@gmail.com>
2014-03-30 20:40:46 -0400

    Updated Javadocs

--- FreddieFinance/src/com/UGAgrads/freddiefinance/CreateAccountPresenter.java
+++ FreddieFinance/src/com/UGAgrads/freddiefinance/CreateAccountPresenter.java
@@ -5,31 +5,41 @@ import android.widget.EditText;
 import android.widget.Spinner;
 
 /**
- * @author
+ * Presenter class used for computations in the CreateAccountActivity class.
+ * 
+ * @author UGAgrads
  */
 public class CreateAccountPresenter {
 	
-    /** FILL THIS IN! */
+    /** SQLite database reference. */
     private static DatabaseHelper db;
-    /** FILL THIS IN! */
+    /** Name of the account that is trying to be created. */
     public static String accountTitle; // must be unique among other accounts of same user
-	/** FILL THIS IN! */
+	/** Starting balance of the account that is trying to be created. */
     public static String balance; // will automatically get formatted to floored to two decimal dollar amount
-	/** FILL THIS IN! */
+	/** Interest rate of the account that is trying to be created. */
     public static String interestRate; 
-	/** FILL THIS IN! */
+	/** Type of account that is trying to be created. */
     public static String accountType; 
 	
-    /** FILL THIS IN! */
+    /** Contains types of errors we can encounter. */
     public static enum FieldError {
-        /** FILL THIS IN! */
-	    NO_ERROR, EMPTY_TITLE, PREXISTING_TITLE, EMPTY_BALANCE, EMPTY_INTEREST,
+        /** No error encountered => all fields have contents. */
+	    NO_ERROR,
+	    /** Account name EditText is empty. */
+	    EMPTY_TITLE, 
+	    /** An account already exists with the specified name. */
+	    PREXISTING_TITLE, 
+	    /** Balance EditText is empty. */
+	    EMPTY_BALANCE, 
+	    /** Interest rate EditText is empty. */
+	    EMPTY_INTEREST,
     }
 
 	/**
 	 * This method handles creation of a user account.
 	 * 
-	 * @param activity FILL THIS IN!
+	 * @param activity Activity calling the method
 	 * @return FieldError representing possible errors
 	 */
     public static FieldError attemptCreate(CreateAccountActivity activity) {
